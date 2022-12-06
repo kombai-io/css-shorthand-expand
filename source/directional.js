c5947db74ad59b64fbf08bbf1edfbccf1c38d58e
Rishi Sanghvi <rishisanghvi@Rishis-MacBook-Pro.local>
2022-12-06 12:38:28 +0530

    feat/border

--- source/directional.js
+++ source/directional.js
@@ -1,8 +1,14 @@
 var repeat = require('repeat-element');
+var isColor = require('./is-color');
 
 module.exports = function (value) {
+  if (isColor(value)) {
+    return ['top', 'right', 'bottom', 'left'].reduce(function (acc, direction, i) {
+      acc[direction] = value;
+      return acc;
+    }, {});
+  }
   var values = value.split(/\s+/);
-
   if (values.length === 1) values = repeat(values[0], 4);
   else if (values.length === 2) values = values.concat(values);
   else if (values.length === 3) values.push(values[1]);
